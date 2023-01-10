258a1290d42aa7df04143f088e2d326fe5a6c256
anyosae <abdoalwaer13@gmail.com>
2023-01-10 20:08:57 +0100

    Create form-validation.js

---
+++ js/form-validation.js
@@ -0,0 +1,43 @@
+$(function() {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("#regform").validate({
+      // Specify validation rules
+      rules: {
+        // The key name on the left side is the name attribute
+        // of an input field. Validation rules are defined
+        // on the right side
+        firstname: {
+            required:true,
+
+        },
+        lastname: "required",
+        email: {
+          required: true,
+          // Specify that email should be validated
+          // by the built-in "email" rule
+          email: true
+          
+        },
+        password: {
+          required: true,
+          minlength: 8
+        }
+      },
+      // Specify validation error messages
+      messages: {
+        firstname: "Please enter your first name",
+        lastname: "Please enter your last name",
+        password: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 8 characters long"
+        },
+        email: "Please enter a valid email address"
+      },
+      // Make sure the form is submitted to the destination defined
+      // in the "action" attribute of the form when valid
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
